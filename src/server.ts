14ea284ec335bad5772959ae1bbe03c86107bfe0
Leonardo Cunha <leonardo.msc42@gmail.com>
2020-12-21 11:21:30 -0300

    style(commit): git commitizen package added
    this package allow us to create standard commit messages through out an interface.

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,5 @@ const app = express();
 app.get('/', (request, response) => response.json({ message: 'Hello World' }));
 
 app.listen(3333, () => {
-  console.log('Server is running on port 3333');
+  console.log('Server is running on port 3333!!!');
 });
